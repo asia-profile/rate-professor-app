8d27bf69f4666f1b5f7c7a0334c62c862aa4504e
Joanna Katarzyna Klara <sc19jkk@leeds.ac.uk>
2022-03-21 17:23:23 +0000

    API and client for 4 commands done. Try once more register login logout

--- client.py
+++ client.py
@@ -2,90 +2,75 @@ from __future__ import print_function, unicode_literals
 import argparse
 import sys
 import requests
-import click
+import json
 
-cli = click.Group()
 
-@cli.command()
 def register():
     json = requests.get('http://127.0.0.1:8000/register').json()
     print(json['register'])
 
 
-@cli.command()
 def login(url):
     json = requests.get(url).json()
     print(json['login'])
 
 
-@cli.command()
 def logut():
-    json = requests.get('http://127.0.0.1:8000/logout').json()
-    print(json['logout'])
+    response = requests.get('http://127.0.0.1:8000/logout')
+    print(json.dumps(response.json(), indent=4))
 
 
-@cli.command()
 def list():
-    json = requests.get('http://127.0.0.1:8000/list').json()
-    print(json['list'])
+    #json = requests.get('http://127.0.0.1:8000/list').json()
+    url = "http://127.0.0.1:8000/list"
+
+    response = requests.get(url)
+    print(json.dumps(response.json(), indent=1))
 
 
-@cli.command()
 def view():
-    json = requests.get('http://127.0.0.1:8000/view').json()
-    print(json['view'])
+    #json = requests.get('http://127.0.0.1:8000/view').json()
+    url = "http://127.0.0.1:8000/view"
+
+    response = requests.get(url)
+    print(json.dumps(response.json(), indent=4))
+
 
 
 #think need to change a bit here, since we're to get that all from command line, so maybe story the *args somewhere
-@cli.command()
+
 def average(professor_id, module_code):
-    url = 'http://127.0.0.1:8000/' + professor_id + '/' + module_code + '/average'
-    json = requests.get(url).json()
-    print(json['average'])
+    url = "http://127.0.0.1:8000/" + professor_id + "/" + module_code + "/average"
+    #json = requests.get(url).json()
+    #print(json['average'])
+    response = requests.get(url)
+    print(json.dumps(response.json(), indent=4))
 
 
-@cli.command()
 def rate(professor_id, module_code, year, semester, rating):
-    url = 'http://127.0.0.1:8000/' + professor_id + '/' + module_code + '/' + year + '/' + semester + '/' + rating + '/rate'
-    json = requests.get(url).json()
-    print(json['rate'])
+    url = 'http://127.0.0.1:8000/' + professor_id + "/" + module_code + "/" + year + "/" + semester + "/" + rating + "/rate"
+    #json = requests.get(url).json()
+    #print(json['rate'])
+    response = requests.get(url)
+    print(json.dumps(response.json(), indent=1))
 
 
-@cli.command()
-#@cli.argument("namespace", nargs=1)
-def process(namespace):
-    print("x")
-
-@cli.command()
-def run(ctx):
-    #for namespace in KEYS.iterkeys():
-    #    ctx.invoke(process, namespace=namespace)
-    for command in sys.argv:
-        if command=="list":
-            ctx.invoke(list)
-        if command=="view":
-            ctx.invoke(view)
-
-
-
-#@click.command()
-#def main():
-#    command_line_argument = sys.argv
+def main():
+    command_line_argument = sys.argv
     #name client.py is taken as sys.argv[0]
     #name of command i.e. list, view, will be second one, and average and rate will be the last ones
-#    print(command_line_argument)
-#    if command_line_argument[1] == "list":
-#        list()
-#    if command_line_argument[1] == "view":
-#        view()
-#    if command_line_argument[1] == "average":
-#        average(command_line_argument[2], command_line_argument[3])
-#    if command_line_argument[1] == "rate":
-#        rate(command_line_argument[2], command_line_argument[3], command_line_argument[4], command_line_argument[5],
-#                command_line_argument[6])
-#    else:
-#        print("Not a supported command")
+    if command_line_argument[1] == "list":
+        list()
+    if command_line_argument[1] == "view":
+        view()
+    if command_line_argument[1] == "average":
+        average(command_line_argument[2], command_line_argument[3])
+    if command_line_argument[1] == "rate":
+        rate(command_line_argument[2], command_line_argument[3], command_line_argument[4], command_line_argument[5],
+                command_line_argument[6])
+    else:
+        print("Not a supported command")
 
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
--- rate_professor_root/events/views.py
+++ rate_professor_root/events/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadReque
 from django.db.models import Sum
 from django.contrib import auth
 from .models import Student, Professor, Module, Rating
+from django.core import serializers
 # Create your views here.
 #this is server side
 
@@ -71,34 +72,30 @@ def list(request): #seems to work, even if not elegant
         http_bad_response.content = 'Only GET requests allowed for this resource\n'
 
     #if reached here, then the request is good; get list of all modules from database
-    modules = Module.objects.all().values('module_code', 'name', 'year', 'semester', 'professors') #returns dictionary
-    #professors = modules.values_list('professors')
-    modules_queryset = Module.objects.all()
-    for m in modules_queryset:
-        professors = m.professors.all().values('firstname', 'lastname', 'professor_id')
+    modules = Module.objects.all().values('module_code', 'name', 'year', 'semester') #returns dictionary
 
     #collect objects and put them in new list items with appropriate json names
     list = []
+
     #have a queryset sth in
     for record in modules:
         #professors = record.professors.all().values('firstname', 'lastname', 'professor_id')
+        module_professors = Module.objects.get(module_code=record['module_code'], year=record['year'], semester=record['semester']).\
+            professors.all().values('firstname', 'lastname', 'professor_id')
 
-        item = {'module_code': record['module_code'], 'module_name': record['name'], 'year': record['year'],
-                'semester': record['semester']} #, 'module_professors': record['professors']}
-        item2 = {'Professor': professors[0]}
+        item = {'module_code': record['module_code'], 'module_name': record['name'], 'year': record['year'], 'semester': record['semester']} #, 'module_professors': record['professors']}
 
-        #shows only id of professor or how many of professors are there or sth and we want to show a list of them
         list.append(item)
-        list.append(item2)
+        for p in module_professors:
+            item2 = {'firstname': p['firstname'], 'lastname': p['lastname'], 'professor_id': p['professor_id'],}
+            list.append(item2)
 
-        #okay now that works for display, even thou it' snot very elegant
 
-    #list.append(modules.values_list('professors'))
     #make final json response payload
     payload = {'module list': list}
     #create and return normal response
     http_response = HttpResponse(json.dumps(payload))
-    http_response['Content Type'] = 'application/json'
+    #http_response['Content Type'] = 'application/json'
     http_response.status_code = 200
     http_response.reason_phrase = 'OK'
     return http_response
@@ -148,21 +145,18 @@ def view(request): #bad numbers: JE gets 3 instead of 4, VS gets 2 instaed of 3;
         list.append(item)
         # make final json response payload
     payload = {'professor ranking list': list}
+
     # create and return normal response
     http_response = HttpResponse(json.dumps(payload))
-    http_response['Content Type'] = 'application/json'
+    #http_response['Content Type'] = 'application/json'
     http_response.status_code = 200
     http_response.reason_phrase = 'OK'
     return http_response
-
-    #return render(request, 'events/professor_rating.html', {"professors": professors})
 
 
 #View the average rating of a certain professor in a certain module
 def average(request, professor_id, module_code):
 
-    #module = Module.objects.filter(module_code=module_code)
-    #ratings = Rating.objects.filter(module_code=module_code, professor_id=professor_id)
     ratings = Rating.objects.filter(professor_id=professor_id)
     ratings_count = 0
     ratings_sum = 0
@@ -179,35 +173,7 @@ def average(request, professor_id, module_code):
     this_professor.rating = average
     this_professor.save()
 
-    #good_ratings = {}
-    #how_many_ratings = 0
-    #for x in ratings:
-    #    if x.module == module_code:
-    #        good_ratings |= x
-    #        how_many_ratings = how_many_ratings + 1
 
-    #professor = Professor.objects.filter(professor_id=professor_id)
-    #rating_count = 1
-    #rating_count = professor.ratings_count
-    #for p in professor:
-    #    if p.professor_id==professor_id and p.ratings_count!=0:
-    #        rating_count = p.ratings_count
-
-    #professor = Professor.objects.get(professor_id=professor_id)
-    #ratings_sum = 0
-    #ratings_count = 0
-    #for r in good_ratings:
-    #    ratings_sum = ratings_sum + r.rating
-        #ratings_count = ratings_count + 1
-
-    #if how_many_ratings!=0:
-    #    average_rating = ratings_sum/how_many_ratings
-    #    average_rating=proper_round(average_rating,0) #round to nearest integer
-    #else:
-    #    average_rating = 1
-
-    #professor.rating = average_rating
-    #professor.save()
     # okay so now that we did the calculation for all the professors about their average - now only similar as in list ig
     # if reached here, then the request is good; get list of all professors from database
     # collect objects and put them in new list items with appropriate json names
@@ -227,20 +193,15 @@ def average(request, professor_id, module_code):
     #create and return normal response
     payload = {'professor in module': list_p}
     http_response = HttpResponse(json.dumps(payload))
-    http_response['Content Type'] = 'application/json'
+    #http_response['Content Type'] = 'application/json'
     http_response.status_code = 201
     http_response.reason_phrase = 'OK'
     return http_response
-    #return render(request, 'events/professor_module_rating.html', {"professor": professor, "average_rating": average_rating, "module": module})
 
 
 #Rate the teaching of a certain professor in a certain module instance.
-#this one I'm not sure about
-#should be here? I think it should, since it goes from server side with calculations...
-#tho from that side it could be that in rate.py, and from client side it would be in myclient.py stuff
 def rate(request, professor_id, module_code, year, semester, rating): #same as above view function...hmmm do we need a form here? Or is it just client from command line
-    # okay, nowe ratings są dodawane - teraz tylko czy mamy zmieniany ich numer u profesora
-    # wygląda na to, że chyba teraz jak coś poprawiłam to ratings count jest zmieniany
+
 
     http_bad_response = HttpResponseBadRequest()
     http_bad_response['Content Type'] = 'text/plain'
@@ -248,18 +209,27 @@ def rate(request, professor_id, module_code, year, semester, rating): #same as a
         http_bad_response.content = 'Only POST/GET requests allowed for this resource\n'
 
 
-    professor = Professor.objects.get(professor_id=professor_id)
+    #professor = Professor.objects.get(professor_id=professor_id)
     module = Module.objects.get(module_code=module_code, year=year, semester=semester)
     #Rating.objects.create(professor_id=request.data["professor_id"],
     #                      module_code=request.data["module_code"],
     #                      rating=request.data["rating"])
-    rating = Rating.objects.create(module=module, professor_id=professor_id, rating=rating)
+    new_rating = Rating.objects.create(module=module, professor_id=professor_id, rating=rating)
+    
+    data_rating = Rating.objects.filter(module=module, professor_id=professor_id, rating=rating).values('professor_id', 'rating')
+    list_r = []
+    for record in data_rating:
+        #item2 = {'module_code': module_code}
+        item = {'module_code': module_code, 'professor': record['professor_id'],
+                'rating': record['rating']}
 
-    professor.save()
+        #list_r.append(item2)
+        list_r.append(item)
 
-    #payload = {'New rating created': list_r}
-    #http_response = HttpResponse(json.dumps(payload))
-    http_response = HttpResponse()
+
+
+    payload = {'New rating created': list_r}
+    http_response = HttpResponse(json.dumps(payload))
     #http_response['Content Type'] = 'application/json'
     http_response.status_code = 201
     http_response.reason_phrase = 'OK'
